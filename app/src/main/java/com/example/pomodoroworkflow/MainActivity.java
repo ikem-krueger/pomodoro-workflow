3bbae2800e2c9e571050435f3c96e238017757ae
Ikem Krueger <ikem.krueger@gmail.com>
2021-03-20 14:01:30 +0100

    Refactor

--- app/src/main/java/com/example/pomodoroworkflow/MainActivity.java
+++ app/src/main/java/com/example/pomodoroworkflow/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.pomodoroworkflow;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -16,6 +21,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Task> tasks = new ArrayList<>();
+    MainAdapter mainAdapter = new MainAdapter(getTaskList(), MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        MainAdapter mainAdapter = new MainAdapter(getTaskList());
         recyclerView.setAdapter(mainAdapter);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(v -> {
-            tasks.add(new Task("25", "focused work", "task"));
-            tasks.add(new Task("5", "short break", "shortBreak"));
 
-            mainAdapter.notifyDataSetChanged();
+        floatingActionButton.setOnClickListener(v -> {
+            showAddDialog(v);
         });
     }
 
@@ -44,5 +47,46 @@ public class MainActivity extends AppCompatActivity {
         tasks.add(new Task("5", "short break", "shortBreak"));
 
         return tasks;
+    }
+
+    public void showAddDialog(final View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Task");
+
+        View viewInflated = LayoutInflater.from(MainActivity.this).inflate(R.layout.activity_dialog, (ViewGroup) findViewById(android.R.id.content), false);
+        builder.setView(viewInflated);
+
+        // Set up the input
+        final EditText edittext_duration = viewInflated.findViewById(R.id.edittext_editactivity_duration);
+        final EditText edittext_description = viewInflated.findViewById(R.id.edittext_editactivity_description);
+        final CheckBox checkBox = viewInflated.findViewById(R.id.checkBox);
+
+        // Set up the cancel button
+        builder.setNegativeButton(getString(R.string.button_cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Set up the update button
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                tasks.add(new Task("25", "focused work", "task"));
+
+                if(checkBox.isChecked()) {
+                    tasks.add(new Task("5", "short break", "shortBreak"));
+                }
+
+                mainAdapter.notifyDataSetChanged();
+
+                dialog.dismiss();
+            }
+        });
+
+        // Show dialog
+        builder.show();
     }
 }
--- app/src/main/java/com/example/pomodoroworkflow/MainAdapter.java
+++ app/src/main/java/com/example/pomodoroworkflow/MainAdapter.java
@@ -1,8 +1,12 @@
 package com.example.pomodoroworkflow;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,9 +17,11 @@ import java.util.ArrayList;
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
 
+    private final MainActivity mainActivity;
     ArrayList<Task> taskList;
 
-    public MainAdapter(ArrayList<Task> taskList) {
+    public MainAdapter(ArrayList<Task> taskList, MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
         this.taskList = taskList;
     }
 
@@ -30,12 +36,13 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
     public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
         Task task = taskList.get(position);
 
-        int taskColor = holder.view.getResources().getColor(R.color.taskColor,null);
-        int pauseColor = holder.view.getResources().getColor(R.color.pauseColor,null);
+        int taskColor = mainActivity.getResources().getColor(R.color.taskColor,null);
+        int pauseColor = mainActivity.getResources().getColor(R.color.pauseColor,null);
 
         switch (task.getType()) {
             case "task":
                 holder.view.setBackgroundColor(taskColor);
+                holder.taskIcon.setImageResource(R.drawable.ic_alarm_black_22dp);
 
                 break;
             case "shortBreak":
@@ -48,6 +55,80 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
 
         holder.taskDescription.setText(task.getDescription());
         holder.taskDuration.setText(task.getDuration() + " minutes");
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Edit " + (task.getType().equals("task") ? "Task" : "Break"));
+
+                View viewInflated = LayoutInflater.from(v.getContext()).inflate(R.layout.activity_dialog, (ViewGroup) v.findViewById(android.R.id.content), false);
+                builder.setView(viewInflated);
+
+                // Set up the input
+                final EditText edittext_description = viewInflated.findViewById(R.id.edittext_editactivity_description);
+                edittext_description.setText(task.getDescription());
+
+                final EditText edittext_duration = viewInflated.findViewById(R.id.edittext_editactivity_duration);
+                edittext_duration.setText(task.getDuration() + " minutes");
+
+                final CheckBox checkBox = viewInflated.findViewById(R.id.checkBox);
+                checkBox.setVisibility(View.GONE);
+
+                // Set up the cancel button
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                // Set up the update button
+                builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        /*
+                        taskList.add(new Task("25", "focused work", "task"));
+
+                        if(checkBox.isChecked()) {
+                            taskList.add(new Task("5", "short break", "shortBreak"));
+                        }
+                        */
+
+                        notifyDataSetChanged();
+
+                        dialog.dismiss();
+                    }
+                });
+
+                // Show dialog
+                builder.show();
+           }
+        });
+
+        holder.itemView.setOnLongClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle("Delete " + (task.getType().equals("task") ? "Task" : "Break"));
+
+            builder.setMessage("Do you want to delete '" + task.getDescription() + "'" + "?");
+
+            builder.setNegativeButton("Cancel", ((dialog, which) -> {
+                dialog.cancel();
+            }));
+
+            builder.setPositiveButton("Delete", (dialog, which) -> {
+                taskList.remove(position);
+
+                notifyDataSetChanged();
+
+                dialog.dismiss();
+            });
+
+            builder.show();
+
+            return true; // NOTE: Why is here a return statement?
+        });
     }
 
     @Override
@@ -58,8 +139,8 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
     public static class MainViewHolder extends RecyclerView.ViewHolder {
         private final View view;
         private final ImageView taskIcon;
-        private final TextView taskDuration;
         private final TextView taskDescription;
+        private final TextView taskDuration;
 
         public  MainViewHolder(View v) {
             super(v);
